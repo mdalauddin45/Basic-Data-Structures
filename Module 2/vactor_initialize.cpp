63d8653ef987261baa1a572d571d126adcba72b4
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-06-06 13:12:12 +0600

    verctor initialize

---
+++ Module 2/vactor_initialize.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //type 1
+    // vector<int>v;
+    // cout<<v.size()<<endl;
+
+    //type 2
+    //  vector<int>v(5);
+    // for(int i=0;i<v.size();i++)
+    // {
+    //     cout<<v[i]<<" ";
+    // }
+    // cout<<endl;
+    // cout<<v.size()<<endl;
+
+    //type 3
+    // vector<int>v(5,10);
+    // for(int i=0;i<v.size();i++)
+    // {
+    //     cout<<v[i]<<" ";
+    // }
+    // cout<<endl;
+    // cout<<v.size()<<endl;
+
+    // vector<int>v2(5,100);
+    // vector<int>v(v2);
+
+    //type 4
+    // for(int i=0;i<v.size();i++)
+    // {
+    //     cout<<v[i]<<" ";
+    // }
+    // cout<<endl;
+    // cout<<v.size()<<endl;
+
+    //type 5
+    int a[6]={1,2,3,4,5,6};
+    vector<int>v(a,a+6);
+    // vector<int> v={4,6,7,9,0};
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
