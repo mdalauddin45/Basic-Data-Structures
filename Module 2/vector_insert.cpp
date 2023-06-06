abf9c3e36046f5303710f52e59b0b05084ca3d82
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-06-06 13:38:25 +0600

    vector insert

---
+++ Module 2/vector_insert.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> v={1,2,3,4,5};
+    vector<int> v2={10,21,2,24,25};
+    v.insert(v.begin()+2,v2.begin(),v2.end());
+    for(int x:v)
+    {
+        cout<<x<<" ";
+    }
+    return 0;
+}
