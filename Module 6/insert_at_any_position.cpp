b805f386d24807a95202d70f6ff59028f4834282
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-06-12 13:35:51 +0600

    insert any position

---
+++ Module 6/insert_at_any_position.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+        int val;
+        Node* next;
+    Node(int val)
+    {
+        this->val=val;
+        this->next=NULL;
+    }
+};
+void insert_at_tail(Node *&head,int v)
+{
+    Node * newNode = new Node(v);
+    if(head==NULL)
+    {
+        head=newNode;
+        return;
+    }
+    //tial
+    Node * temp=head;
+    while (temp->next!=NULL)
+    {
+       temp=temp->next;
+    }
+    //temp ekhon last node a
+    temp->next=newNode;
+    cout<<endl<<"Inserted at tail"<<endl<<endl;
+}
+void print_linked_list(Node *head)
+{
+    cout<<"Your Linked List ";
+    Node*temp=head;
+    while (temp!=NULL)
+    {
+        cout<<temp->val<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+void insert_at_position(Node * head ,int pos,int v)
+{
+    Node* newNode = new Node(v);
+    Node* temp=head;
+    for(int i=1;i<=pos-1;i++)
+    {
+        temp= temp->next;
+    }
+    newNode->next=temp->next;
+    temp->next=newNode;
+    cout<<endl<<endl<<"Inserted at position "<<pos<<endl;
+}
+int main()
+{
+    Node* head=NULL;
+    while (true)
+    {
+        cout<<"Option 1: Insert at Tail"<<endl;
+        cout<<"Option 2: Print Linked List "<<endl;
+        cout<<"Option 3: Inser at any position "<<endl;
+        cout<<"Option 4: Terminate "<<endl;
+        int op;
+        cin>>op;
+        if(op==1)
+        {
+        cout<<"Enter Value : ";
+        int v;
+        cin>>v;
+        insert_at_tail(head,v);
+        }
+        else if (op==2)
+        {
+            print_linked_list(head);
+        }
+        else if (op==3)
+        {
+            int pos,v;
+            cout<<"Enter position: ";
+            cin>>pos;
+            cout<<"Enter Value ";
+            cin>>v;
+            insert_at_position(head,pos,v);
+        }
+        else if (op==4)
+        {
+           break;
+        }   
+        
+    }
+    
+    return 0;
+}
