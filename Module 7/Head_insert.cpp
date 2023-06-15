95832c42ac6ce6d5fc1d88c04feca462cc4e0d41
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-06-15 12:56:38 +0600

    insert head

--- Module 7/Head_insert.cpp
+++ Module 7/Head_insert.cpp
@@ -21,6 +21,21 @@ void Print_link_list(Node * head) //O(n)
     }
     cout<<endl;
 }
+void insert(Node*&head,int v)
+{
+    Node * newNode = new Node(v);
+    if(head==NULL)
+    {
+        head=newNode;
+        return;
+    }
+    Node * temp = head;
+    while (temp->next!=NULL)
+    {
+        temp= temp->next; // i++;
+    }
+    temp->next=newNode;
+}
 void insert_pos(Node *&head,int pos, int v ) //O(n)
 {
     Node * newNode = new Node(v);
@@ -32,6 +47,11 @@ void insert_pos(Node *&head,int pos, int v ) //O(n)
     // temp = pos-1 gory achy   // cout<<temp->val;
     newNode->next=temp->next;
     temp->next=newNode;
+}
+void insert_head(Node *&head,int v){ //O(1) kono loop na chalaily O(1)
+    Node * newNode = new Node(v);
+    newNode->next=head;
+    head=newNode;
 }
 int size(Node * head) // O(n) - n=size
 {
@@ -59,14 +79,19 @@ int main()
     b->next=c;
     c->next=d;
 
+    Print_link_list(head);
      int pos,val;
      cin>>pos>>val;
      if(pos>size(head))
      {
         cout<<"Invalid Index"<<endl;
-     }else{
-        insert_pos(head,pos,val);
-        Print_link_list(head);
+     }else if(pos==0)
+     {
+        insert_head(head,val);
      }
+     else{
+        insert_pos(head,pos,val);
+     }
+    Print_link_list(head);
     return 0;
 }
