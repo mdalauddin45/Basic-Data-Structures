a300684c646a14882b080deba278cc870053f052
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-06-15 10:45:09 +0600

    linklist

--- Module 7/Singly_linked_list.cpp
+++ Module 7/Singly_linked_list.cpp
@@ -26,8 +26,28 @@ void insert(Node*&head,int v)
     }
     temp->next=newNode;
 }
+void print_Link_List(Node*head)
+{
+    Node * temp = head;
+    while (temp!=NULL)
+    {
+        cout<<temp->val<<" ";
+        temp= temp->next;
+    }
+}
 int main()
 {
-    
+    Node * head = new Node(10);
+    Node * a = new Node(20);
+    Node * b = new Node(30);
+    Node * c = new Node(40);
+    Node * d = new Node(50);
+
+    head->next=a;
+    a->next=b;
+    b->next=c;
+    c->next=d;
+    // cout<<head->val<<endl;
+    print_Link_List(head);
     return 0;
 }
