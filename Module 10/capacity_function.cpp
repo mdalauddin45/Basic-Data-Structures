51de1166846cf294d3c7bbb8e41bb443a2b872fe
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-06-18 23:48:00 +0600

    capacity function

---
+++ Module 10/capacity_function.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    list<int>mylist = {10,20,30};
+    // cout<<mylist.max_size();
+    // mylist.clear();
+    // mylist.resize(6,5);
+
+    for(int val:mylist)
+    {
+        cout<<val<<" ";
+    }
+    return 0;
+}
