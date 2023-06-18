8e563465281a068f46bf3a28635d802784558238
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-06-18 18:20:08 +0600

    error solved

--- Module 9/Insert_head_&_tail.cpp
+++ Module 9/Insert_head_&_tail.cpp
@@ -59,7 +59,19 @@ void insert_head(Node*&head,Node* &tail,int v)
     head->pre=newNode;
     head=newNode;
 }
-
+void insert_tail(Node*&head,Node*&tail,int v)
+{
+    Node * newNode = new Node (v);
+    if(tail==NULL)
+    {
+        tail=newNode;
+        head=newNode;
+        return;
+    }
+    tail->next=newNode;
+    newNode->pre=tail;
+    tail=tail->next;
+}
 int size(Node* head)
 {
     int i=0;
@@ -72,26 +84,32 @@ int size(Node* head)
 }
 int main()
 {
-    Node* head= new Node(10);
-    Node* a= new Node(20);
-    Node* b= new Node(30);
-    Node* c= new Node(40);
-    Node* tail=c;
+    Node* head= NULL;
+    Node* tail=NULL;
+    // Node* head= new Node(10);
+    // Node* a= new Node(20);
+    // Node* b= new Node(30);
+    // Node* c= new Node(40);
+    // Node* tail=c;
 
     //connection
-    head->next=a;
-    a->pre=head;
-    a->next=b;
-    b->pre=a;
-    b->next=c;
-    c->pre=b;
+    // head->next=a;
+    // a->pre=head;
+    // a->next=b;
+    // b->pre=a;
+    // b->next=c;
+    // c->pre=b;
 
     int val,pos;
     cin>>val>>pos;
     if(pos==0)
     {
         insert_head(head,tail,val);
-    }else if(pos>=size(head))
+    }
+    else if(pos==size(head)){
+        insert_tail(head, tail,val);
+    }
+    else if(pos>=size(head))
     {
         cout<<"Invalid"<<endl;
     }else{
