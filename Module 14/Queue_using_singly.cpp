43c05e309db07753c1d3cc1d818f7dce201280fc
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-03 10:17:51 +0600

    Queue using singly linked list

--- Module 14/Queue_using_singly.cpp
+++ Module 14/Queue_using_singly.cpp
@@ -16,8 +16,10 @@ class myQueue
     public:
     Node* head = NULL;
     Node* tail= NULL;
+    int sz=0;
     void push(int val)
     {
+        sz++;
         Node* newNode = new Node(val);
         if(head==NULL)
         {
@@ -28,9 +30,69 @@ class myQueue
         tail->next=newNode;
         tail=tail->next;
     }
+    void pop()
+    {
+       sz--;
+        Node *deleteNode = head;
+        head = head->next;
+        delete deleteNode;
+        if (head == NULL)
+        {
+            tail = NULL;
+        }
+    }
+    int front()
+    {
+        return head->val;
+    }
+    int size()
+    {
+        return sz;
+    }
+    bool empty()
+    {
+        if (sz == 0)
+            return true;
+        else
+            return false;
+    }
 };
 int main()
 {
-    
+    // myQueue q;
+    // q.push(10);
+    // q.push(20);
+    // q.push(30);
+    // cout<<q.front()<<endl;
+    // q.pop();
+    // cout<<q.front()<<endl;
+    // q.pop();
+    // cout<<q.front()<<endl;
+    // q.pop();
+    // cout<<q.front()<<endl;
+    // q.pop();
+
+    // while (!q.empty())
+    // {
+    //     cout<<q.front()<<" ";
+    //     q.pop();
+    // }
+
+    // Queue input
+
+    myQueue s;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        s.push(x);
+    }
+    while (!s.empty())
+    {
+        cout<<s.front()<<" ";
+        s.pop();
+    }
     return 0;
 }
