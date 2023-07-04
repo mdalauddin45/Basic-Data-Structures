788e91128086969568ed6014746d8d28cb24b46c
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-04 22:32:31 +0600

    problem 3 solved

---
+++ Module 14.5/Problem_3.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+class myStack{
+    public:
+    list<int>l;
+    void push(int val)
+    {
+        l.push_back(val);
+    }
+    void pop()
+    {
+        l.pop_front();
+    }
+    int top()
+    {
+        return l.front();
+    }
+    int size()
+    {
+        return l.size();
+    }
+    bool empty()
+    {
+        return l.empty();
+    }
+};
+int main()
+{
+    myStack st;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        st.push(x);
+    }
+    while (!st.empty())
+    {
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+    return 0;
+}
