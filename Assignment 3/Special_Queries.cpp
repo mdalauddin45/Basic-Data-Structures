8ea23b99016c4b8f2146d3154cd748ec673f0bb0
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-06 21:15:25 +0600

    special queries solved

---
+++ Assignment 3/Special_Queries.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    queue<string>line;
+    int q;
+    cin>>q;
+    while (q--)
+    {
+        int x;
+        cin>>x;
+        if(x==0)
+        {
+            string name;
+            cin>>name;
+            line.push(name);
+        }
+        else if (x==1)
+        {
+            if(!line.empty())
+            {
+                cout<<line.front()<<endl;
+                line.pop();
+            }
+            else
+            {
+                cout<<"Invalid"<<endl;
+            }
+        }
+        
+        
+    }
+    
+    return 0;
+}
