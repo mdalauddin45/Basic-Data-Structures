d7c6e1209f9de6978e72d6bf8674d6ea5869cf16
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-10 11:03:13 +0600

    bnode create

---
+++ Module 17/Binary_Search_impliment.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+    Node(int val)
+    {
+        this->val=val;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+int main()
+{
+    
+    return 0;
+}
