713a40d1ba461170d0de9c84f568cf69c776e4f2
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-13 21:10:54 +0600

    Sum of tree

---
+++ Assignment 4/Sum_of_Tree.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+    Node(int val)
+    {
+        this->val=val;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+        int l, r;
+        cin >> l >> r;
+        Node *myLeft;
+        Node *myRight;
+        if (l == -1)
+            myLeft = NULL;
+        else
+            myLeft = new Node(l);
+        if (r == -1)
+            myRight = NULL;
+        else
+            myRight = new Node(r);
+
+        p->left = myLeft;
+        p->right = myRight;
+
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+    return root;
+}
+int Sum_of_Tree(Node*root)
+{
+    if(root==NULL) return 0;
+    return root->val+Sum_of_Tree(root->left)+Sum_of_Tree(root->right);
+}
+int main()
+{
+    Node *root = input_tree();
+    cout<<Sum_of_Tree(root)<<endl;
+    return 0;
+}
