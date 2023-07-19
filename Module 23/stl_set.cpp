a9c74f299f5d3f88eca5b7747a9275b440d71970
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-19 23:50:40 +0600

    insert input

--- Module 23/stl_set.cpp
+++ Module 23/stl_set.cpp
@@ -3,12 +3,21 @@ using namespace std;
 int main()
 {
     set<int> st;
-    st.insert(15);
-    st.insert(50);
-    st.insert(15);
-    st.insert(50);
-    st.insert(45);
-    st.insert(45);
+    // st.insert(15);
+    // st.insert(50);
+    // st.insert(15);
+    // st.insert(50);
+    // st.insert(45);
+    // st.insert(45);
+
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        st.insert(x);
+    }
 
 
     for(auto it=st.begin(); it!=st.end(); it++)
