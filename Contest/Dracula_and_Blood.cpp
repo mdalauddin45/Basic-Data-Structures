c80864eea38bbaa76fbb2db359d65845c5707685
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-07-29 21:27:06 +0600

    not solve

---
+++ Contest/Dracula_and_Blood.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int T;
+    cin >> T;
+    cin.ignore();
+
+    for (int t = 0; t < T; t++) {
+        string binary_string;
+        getline(cin, binary_string);
+        // i wann for each test case, output a single line       
+        // containing the minimum number of operations needed to convert binary_string to a string of all 0s.
+        // int count = 0;
+        // for (int i = 0; i < binary_string.length(); i++) {
+        //     if (binary_string[i] == '1') {
+        //         count++;
+        //         while (binary_string[i] == '1') {
+        //             i++;
+        //         }
+        //     }
+        // } 
+        // i wanna For each test case, number of bottles of milk Zaza drank.
+
+    }
+
+    return 0;
+}
